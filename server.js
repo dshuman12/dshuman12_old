ed94bd3ba113cafcfbcb84bea9d488474d2b48de
dshuman12 <30298537+dshuman12@users.noreply.github.com>
2020-01-21 19:17:30 -0500

    Fix for Missing Background Image

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express')
-
 const app = express()
-app.set('view engine', 'ejs')
+const path = require('path');
+const router = express.Router();
+
+app.set('view engine', 'html')
 
 app.use('/views/', express.static('./views'));
 app.use('/public/images/', express.static('./public/images'));
+app.use('/', router);
 
-app.get('/', function (req, res) {
-    res.render('index');
-  })
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
